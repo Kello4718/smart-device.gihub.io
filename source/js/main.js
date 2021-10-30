4ab89c641c2be9a24373d622211f03dc7949db91
Сергей <77622952+Kello4718@users.noreply.github.com>
2021-10-30 13:30:39 +0500

    Add files via upload

--- source/js/main.js
+++ source/js/main.js
@@ -89,9 +89,9 @@ modal.addEventListener('click', (evt) => {
 
 modalForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  localStorage.setItem('modalName', modalInputName.value);
-  localStorage.setItem('modalTel', modalInputPhone.value);
-  localStorage.setItem('modalQuestion', modalTextareaQuestion.value);
+  localStorage.setItem('name', modalInputName.value);
+  localStorage.setItem('phone', modalInputPhone.value);
+  localStorage.setItem('question', modalTextareaQuestion.value);
   modalInputName.value = '';
   modalInputPhone.value = '';
   modalTextareaQuestion.value = '';
@@ -99,11 +99,13 @@ modalForm.addEventListener('submit', (evt) => {
   bodyUnfixPosition();
 });
 
+// Обработчик события на отправку формы
+
 callbackForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  localStorage.setItem('formName', callbackInputName.value);
-  localStorage.setItem('formTel', callbackInputPhone.value);
-  localStorage.setItem('formQuestion', callbackTextareaQuestion.value);
+  localStorage.setItem('name', callbackInputName.value);
+  localStorage.setItem('phone', callbackInputPhone.value);
+  localStorage.setItem('question', callbackTextareaQuestion.value);
   callbackInputName.value = '';
   callbackInputPhone.value = '';
   callbackTextareaQuestion.value = '';
